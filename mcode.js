cc37c8c2d463b5ef9c90d911d26f65144bde3f4d
romana1990 <74625995+romana1990@users.noreply.github.com>
2023-11-27 20:03:41 +0100

    Update mcode.js

--- mcode.js
+++ mcode.js
@@ -6,7 +6,7 @@
             },
             t = {};
         for (const e in r) t[e] = btoa(document.querySelector(r[e]).value).replace(/=/g, "");
-        return "dnMwMzNvag==" !== t.u ? alert("Ups. Try again.") : window.location.href = 'SFBM.html';
+        return "dnMwMzNvag==" !== t.u ? alert("Ups. Try again.") : window.location.href = 'asocijacije.html';
     }))
 })();
 
