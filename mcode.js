df554d16c66ecca63242e932579be92cc4bb926c
romana1990 <74625995+romana1990@users.noreply.github.com>
2023-11-27 20:01:19 +0100

    Add files via upload

---
+++ mcode.js
@@ -0,0 +1,13 @@
+(async () => {
+    await new Promise((e => window.addEventListener("load", e))), document.querySelector("form").addEventListener("submit", (e => {
+        e.preventDefault();
+        const r = {
+                u: "input[name=P1]",
+            },
+            t = {};
+        for (const e in r) t[e] = btoa(document.querySelector(r[e]).value).replace(/=/g, "");
+        return "dnMwMzNvag==" !== t.u ? alert("Ups. Try again.") : window.location.href = 'SFBM.html';
+    }))
+})();
+
+
